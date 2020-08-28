b1f4bc56f0ad00e26686bdec7f0d3f2e16002ad3
TysonKerr <tysonkerr117@gmail.com>
2020-08-28 19:06:17 -0400

    added more tests

--- tests/basic.php
+++ tests/basic.php
@@ -23,8 +23,25 @@ $tests = [
     ['false true', [false, true]],
     ['false true null', [false, true, null]],
     ['false null true', [false, null, true]],
+    ['a:b:c:cat a:b:c:cut a:b:d:[dog] a:b:e:egg', ['a' => ['b' => ['c' => 'cut', 'd' => ['dog'], 'e' => 'egg']]]],
 ];
 
 foreach ($tests as $i => $test) {
     test_tcon("Basic $i", $test[0], $test[1]);
+}
+
+$json_tests = [
+    'null',
+    'true',
+    'false',
+    ' null ',
+    ' true ',
+    ' false ',
+    '["a", "b"]',
+    '["a", "b", ["a"], {"a": "apple"}]',
+    '{"a": {"b" : {"c":["cat", "cut"]}}}',
+];
+
+foreach ($json_tests as $i => $test) {
+    test_tcon("JSON $i:", $test, json_decode($test, true));
 }
